70de292b0faa864b95a7f55dfc380d2bff2887d9
MartinKatona <martin.katona97@gmail.com>
2022-09-18 18:06:58 +0200

    separate DarkMode

---
+++ src/components/DarkMode.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="text-center">
+    <toggle v-model="darkMode" class="dense"></toggle>
+    <!-- <label id="toggle-label" class="text-green-500"> Dark mode </label> -->
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, watch } from "vue";
+
+let darkMode = ref<boolean>(localStorage.theme === "dark");
+if (darkMode.value) document.documentElement.classList.add("dark");
+
+watch(darkMode, () => {
+  if (darkMode.value) {
+    document.documentElement.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    document.documentElement.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+</script>
